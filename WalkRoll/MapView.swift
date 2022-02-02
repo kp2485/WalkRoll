8ed6b07b05ca5ebc6117b1a6d0844e7d41557450
Kyle Peterson <kdpeters@gmail.com>
2022-02-02 16:16:05 -0500

    added MapView

---
+++ WalkRoll/MapView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Walk&Roll
+//
+//  Created by Kyle Peterson on 1/31/22.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapView: View {
+    @StateObject private var viewModel = MapViewModel()
+    
+    var body: some View {
+        Map(coordinateRegion: $viewModel.region, showsUserLocation: true )
+            .ignoresSafeArea()
+            // dot color
+            .accentColor(Color(.systemPink))
+            .onAppear {
+                viewModel.checkIfLocationServicesIsEnabled()
+            }
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
---
+++ WalkRoll/MapViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  MapViewModel.swift
+//  Walk&Roll
+//
+//  Created by Kyle Peterson on 1/31/22.
+//
+
+import MapKit
+
+enum MapDetails {
+    static let startingLocation = CLLocationCoordinate2D (latitude: 42.3314, longitude: 83.0458)
+}
+
+final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    @Published var region = MKCoordinateRegion(center: MapDetails.startingLocation, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    
+    var locationManager: CLLocationManager?
+    
+    func checkIfLocationServicesIsEnabled() {
+        if CLLocationManager.locationServicesEnabled() {
+            // Initialize locationManager as an optional with best location accuracy
+            locationManager = CLLocationManager()
+            locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+            // Set the delegate and force unwraps it
+            locationManager!.delegate = self
+        } else {
+// FIX
+            print ("Prompt user to change location settings")
+        }
+    }
+    
+    private func checkLocationAuthorization() {
+        // Unwrap Optional
+        guard let locationManager = locationManager else { return }
+        
+        switch locationManager.authorizationStatus {
+
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Your location is restricted likely due to parental controls")
+        case .denied:
+            print("You have denied this app location permission. Go into settings to change the permission.")
+        case .authorizedAlways, .authorizedWhenInUse:
+            // update region, force unwrapped optional
+            region = MKCoordinateRegion(center: locationManager.location!.coordinate,
+                                        span: MKCoordinateSpan( latitudeDelta: 0.01, longitudeDelta: 0.01))
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+        
+}
