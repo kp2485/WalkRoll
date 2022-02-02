fd1bbb93471fec09d91fc61344140699b7f9d706
Kyle Peterson <kdpeters@gmail.com>
2022-02-02 13:49:10 -0500

    Initial Commit

---
+++ WalkRoll/WalkRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkRollApp.swift
+//  WalkRoll
+//
+//  Created by Kyle Peterson on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WalkRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
