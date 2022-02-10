99e81efd37898a3e369ce19a23a7e7109de47605
Kyle Peterson <kdpeters@gmail.com>
2022-02-10 12:08:30 -0500

    added button to authorize access to HealthKit for step

--- WalkRoll/ShortProgressView.swift
+++ WalkRoll/ShortProgressView.swift
@@ -8,13 +8,90 @@
 import HealthKit
 import SwiftUI
 
+func fetchHealthData() -> Void {
+    let healthStore = HKHealthStore()
+    if HKHealthStore.isHealthDataAvailable() {
+        let readData = Set([
+            HKObjectType.quantityType(forIdentifier: .heartRate)!
+        ])
+        
+        healthStore.requestAuthorization(toShare: [], read: readData) { (success, error) in
+            if success {
+                let calendar = NSCalendar.current
+                
+                var anchorComponents = calendar.dateComponents([.day, .month, .year, .weekday], from: NSDate() as Date)
+                
+                let offset = (7 + anchorComponents.weekday! - 2) % 7
+                
+                anchorComponents.day! -= offset
+                anchorComponents.hour = 2
+                
+                guard let anchorDate = Calendar.current.date(from: anchorComponents) else {
+                    fatalError("*** unable to create a valid date from the given components ***")
+                }
+                
+                let interval = NSDateComponents()
+                interval.minute = 30
+                
+                let endDate = Date()
+                
+                guard let startDate = calendar.date(byAdding: .month, value: -1, to: endDate) else {
+                    fatalError("*** Unable to calculate the start date ***")
+                }
+                
+                guard let quantityType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate) else {
+                    fatalError("*** Unable to create a step count type ***")
+                }
+                
+                let query = HKStatisticsCollectionQuery(quantityType: quantityType,
+                                                        quantitySamplePredicate: nil,
+                                                        options: .discreteAverage,
+                                                        anchorDate: anchorDate,
+                                                        intervalComponents: interval as DateComponents)
+                
+                query.initialResultsHandler = {
+                    query, results, error in
+                    
+                    guard let statsCollection = results else {
+                        fatalError("*** An error occurred while calculating the statistics: \(String(describing: error?.localizedDescription)) ***")
+                        
+                    }
+                    
+                    statsCollection.enumerateStatistics(from: startDate, to: endDate) { statistics, stop in
+                        if let quantity = statistics.averageQuantity() {
+                            let date = statistics.startDate
+                            //for: E.g. for steps it's HKUnit.count()
+                            let value = quantity.doubleValue(for: HKUnit(from: "count/min"))
+                            print("done")
+                            print(value)
+                            print(date)
+                            
+                        }
+                    }
+                    
+                }
+                
+                healthStore.execute(query)
+                
+            } else {
+                print("Authorization failed")
+                
+            }
+        }
+    }
+}
 struct ShortProgressView: View {
     var body: some View {
-        
-        
-        
-        
-        Text("456 Steps Walked Today").frame(height: 200)
+        Button(action: fetchHealthData) {
+            Text("Fetch steps")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.white)
+            
+        }
+        .padding()
+        .background(Color.blue)
+        .cornerRadius(20)
     }
 }
 
