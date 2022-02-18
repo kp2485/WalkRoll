e1f01a4696cb84c5f3c2f44adc21cc7f4063455c
Kyle Peterson <kdpeters@gmail.com>
2022-02-18 14:36:21 -0500

    redesigned UI

--- WalkRoll/ContentView.swift
+++ WalkRoll/ContentView.swift
@@ -20,43 +20,94 @@ struct ContentView: View {
                     MapView()
                         .frame(height: screenWidth)
                     
-                    ZStack {
-                        LinearGradient(gradient: Gradient(colors: [.blue, .white, .green]), startPoint: .top, endPoint: .bottom)
-                            .frame(height:50)
-                            .opacity(0.35)
-                        HStack {
-                            Image(systemName: "figure.walk")
-                            Text("Upcoming Walk & Rolls")
-                                .font(.title)
-                                .fontWeight(.medium)
-                            Image(systemName: "figure.roll")
-                        }
-                    }
+                    Text("Upcoming Walk & Rolls")
+                        .font(.title)
+                        .fontWeight(.medium)
+                        .padding(.horizontal)
+                        .padding(.vertical, 1)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
                     
                     List {
                         NavigationLink(destination: JoinAWalkAndRoll()) {
                             HStack {
                                 Text("Boston-Edison AM Crew")
                                     .font(.title2)
+                                    .layoutPriority(1)
+                                    .lineLimit(2)
                                 Spacer()
                                 Text("0.2 m")
+                                    .font(.body)
                                     .fontWeight(.light)
+                                    .lineLimit(1)
                             }
+                            .minimumScaleFactor(0.5)
                         }
                         NavigationLink(destination: PlanAWalkAndRoll()) {
                             HStack {
                                 Text("Sacred Heart Track")
                                     .font(.title2)
+                                    .layoutPriority(1)
+                                    .lineLimit(2)
                                 Spacer()
                                 Text("0.5 m")
+                                    .font(.body)
                                     .fontWeight(.light)
+                                    .lineLimit(1)
                             }
+                            .minimumScaleFactor(0.5)
                         }
-                        
+                        NavigationLink(destination: PlanAWalkAndRoll()) {
+                            HStack {
+                                Text("Central HS Track")
+                                    .font(.title2)
+                                    .layoutPriority(1)
+                                    .lineLimit(2)
+                                Spacer()
+                                Text("0.5 m")
+                                    .font(.body)
+                                    .fontWeight(.light)
+                                    .lineLimit(1)
+                            }
+                            .minimumScaleFactor(0.5)
+                        }
+                        NavigationLink(destination: PlanAWalkAndRoll()) {
+                            HStack {
+                                Text("West Side Walkers")
+                                    .font(.title2)
+                                    .layoutPriority(1)
+                                    .lineLimit(2)
+                                Spacer()
+                                Text("2.2 m")
+                                    .font(.body)
+                                    .fontWeight(.light)
+                                    .lineLimit(1)
+                            }
+                            .minimumScaleFactor(0.5)
+                        }
+                        NavigationLink(destination: PlanAWalkAndRoll()) {
+                            HStack {
+                                Text("Palmer Park")
+                                    .font(.title2)
+                                    .layoutPriority(1)
+                                    .lineLimit(2)
+                                Spacer()
+                                Text("3.7 m")
+                                    .font(.body)
+                                    .fontWeight(.light)
+                                    .lineLimit(1)
+                            }
+                            .minimumScaleFactor(0.5)
+                        }
+                        HStack {
+                            Spacer()
+                            Text("See more...")
+                            Spacer()
+                        }
                     }
                     .onAppear(perform: {
-                            UITableView.appearance().contentInset.top = -15
-                        })
+                        UITableView.appearance().contentInset.top = -15
+                    })
                     
                     
                 }
@@ -75,8 +126,11 @@ struct ContentView: View {
                             
                             Spacer()
                             
-                            Text ("Welcome, Otis!")
+                            Text ("Welcome, Poindexter!")
                                 .font(.largeTitle)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.5)
+                                .frame(maxHeight: .infinity)
                             
                             Spacer()
                             
@@ -86,18 +140,19 @@ struct ContentView: View {
                             }
                             
                         }
+                        .background()
                         
                     }
-//                    ToolbarItem(placement: .navigationBarLeading) {
-//                        NavigationLink(destination: MySettings()) {
-//                            Text ("My Settings")
-//                        }
-//                    }
-//                    ToolbarItem(placement: .navigationBarTrailing) {
-//                        NavigationLink(destination: MySettings()) {
-//                            Text ("Plan a Walk & Roll")
-//                        }
-//                    }
+                    //                    ToolbarItem(placement: .navigationBarLeading) {
+                    //                        NavigationLink(destination: MySettings()) {
+                    //                            Text ("My Settings")
+                    //                        }
+                    //                    }
+                    //                    ToolbarItem(placement: .navigationBarTrailing) {
+                    //                        NavigationLink(destination: MySettings()) {
+                    //                            Text ("Plan a Walk & Roll")
+                    //                        }
+                    //                    }
                 }
             }
             //            Text("Today is a great day to get outside! ⛅️")
@@ -119,8 +174,10 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .environment(\.sizeCategory, .large)
+            .environment(\.sizeCategory, .medium)
+            
         ContentView()
+        
             .environment(\.sizeCategory, .accessibilityExtraExtraExtraLarge)
     }
 }
