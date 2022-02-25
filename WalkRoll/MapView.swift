c54830b3f6100d9ef1050802075fada73ed928f5
Kyle Peterson <kdpeters@gmail.com>
2022-02-25 09:20:29 -0500

    animated circles for W&Rs

--- WalkRoll/MapView.swift
+++ WalkRoll/MapView.swift
@@ -10,15 +10,25 @@ import SwiftUI
 
 struct MapView: View {
     @StateObject var viewModel = MapViewModel()
+    @State var isAnimating: Bool = false
+    
     var walkRolls: [WalkRoll]
     
     var body: some View {
         Map(coordinateRegion: $viewModel.region, showsUserLocation: true, annotationItems: walkRolls) { annotation in
             MapAnnotation(coordinate: annotation.coordinate) {
-                    Image(systemName: "location.magnifyingglass")
-                        .frame(width: 30, height: 30)
-                        .offset(x: 2, y: 2)
-                        .foregroundColor(.primary)
+//                    Image(systemName: "location.magnifyingglass")
+//                        .frame(width: 30, height: 30)
+//                        .offset(x: 2, y: 2)
+//                        .foregroundColor(.primary)
+                Circle()
+                    .strokeBorder(Color.blue, lineWidth: isAnimating ? 5:1)
+                    .animation(.linear(duration: 1.5).repeatForever(), value: isAnimating)
+                    .frame(width: 30, height: 30)
+                    .onAppear {
+                        isAnimating = true
+                    }
+                        
             }
         }
             .ignoresSafeArea()
